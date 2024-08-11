9103478da541d4629d2113c7269da3d5907d7852
Unnat43300 <12203219@students.koi.edu.au>
2024-08-11 21:57:56 +1000

    Add project files.

---
+++ OddandEven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Input number
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        // Check if the number is odd or even
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("{0} is an even number.", num);
+        }
+        else
+        {
+            Console.WriteLine("{0} is an odd number.", num);
+        }
+    }
+}
